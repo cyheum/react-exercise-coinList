4d84ef78f73168de20169060980aefce27c6c55b
최예흠 <coffee8357@gmail.com>
2020-11-06 15:51:40 +0900

    오타 수정 완료

---
+++ src/components/molecules/Molecule.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Molecule = () => {
+  return <div>Molecule</div>;
+};
---
+++ src/components/organisms/Organism.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Organism = () => {
+  return <div>Organism</div>;
+};
